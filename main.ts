5ec27c56f516013e665fd06fda506b43c224e5aa
mubarak <abubakrobasanjo@gmail.com>
2025-04-13 11:50:51 -0700

    Initial commit

---
+++ main.ts
@@ -0,0 +1,32 @@
+// number type 
+let mynumb = 10
+let anothernumb: number = 20
+
+mynumb = 30
+anothernumb = '40'
+
+// string and boolean type
+let mystr: string = "hello"
+let anotherStr = "world"
+
+mystr = true
+anotherStr = 'fatimoh'
+
+// boolean type
+let mybool: boolean = false
+let anotherBool = false
+
+mybool = true
+anotherBool = 'hi'
+
+// inferred type
+let salary;
+salary= 12000
+salary = '12000'
+salary = true
+
+// inferred
+let salary:number;
+salary = 12000;
+salary = '12000';
+salary = true;
